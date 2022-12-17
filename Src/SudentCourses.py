7da0a1d37c52c78a124defe31c5618c61a81ad59
sara <saraamersaad8@gmail.com>
2022-12-17 22:33:19 +0200

    feat: add options to add , delete and update student

---
+++ Src/SudentCourses.py
@@ -0,0 +1,7 @@
+class StudentCourses:
+    def __int__(self):
+        self.course_id = 0
+        self.student_id = 0
+        self.grade = ''
+        self.next_student = None
+        self.next_course = None
--- Src/main.py
+++ Src/main.py
@@ -1,16 +1,27 @@
 import Courses
-choice = 167
+import Students
+choice = 1
+course = Courses.Courses()
+students = Students.Student()
 while choice != 0:
     print('1) List Courses')
     print('2) Add New Course')
     print('3) Update Course')
     print('4) Delete Course')
+    print('5) List Students')
+    print('6) Add New Student')
+    print('7) Update Student')
+    print('8) Delete Student')
     choice = int(input('What do you want to do?'))
     print(choice, 'choice')
-    course = Courses.Courses()
     switcher = {
         1: course.print_list,
         2: course.add_new_course,
-        3: course.update_course
+        3: course.update_course,
+        4: course.delete_course,
+        5: students.print_list,
+        6: students.add_new_student,
+        7: students.update_student,
+        8: students.delete_student
     }
-    switcher[choice]()
+    switcher.get(choice, 'invalid')()
